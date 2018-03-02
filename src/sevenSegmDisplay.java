06045ae304a3e46959743f3abaa53101cdc91459
osxisl <olehumc@gmail.com>
2018-03-02 23:32:13 +0200

    sevenDigitsDisplay class

--- src/sevenSegmDisplay.java
+++ src/sevenSegmDisplay.java
@@ -1,43 +1,29 @@
+import java.util.Scanner;
+
 public class sevenSegmDisplay {
     public static void main(String[] args) {
+        System.out.println("Enter number to display: ");
+        Scanner in = new Scanner(System.in);
+        long number = in.nextLong();
 
+        String numberStr = String.valueOf(number);
+        int[] numberArray = new int [numberStr.length()];
+        for (int i=0; i<numberStr.length(); i++) {
+            numberArray[i] = Integer.parseInt( String.valueOf(numberStr.charAt(i)) );
+        }
+
+        String[][] symbolsArray = {
+                {"._. ", "... ", "._. ", "._. ", "... ", "._. ", "._. ", "._. ", "._. ", "._. "},
+                {"|.| ", "..| ", "._| ", "._| ", "|_| ", "|_. ", "|_. ", "..| ", "|_| ", "|_| "},
+                {"|_| ", "..| ", "|_. ", "._| ", "..| ", "._| ", "|_| ", "..| ", "|_| ", "..| "},
+                };
+
+        for (int i=0; i<3; i++){
+            for ( int DigitsNumber = 0; DigitsNumber<numberStr.length();DigitsNumber++) {
+                int num = numberArray[DigitsNumber];
+                System.out.print(symbolsArray[i][num]);
+            }
+            System.out.println("");
+        }
     }
 }
-
-
-/*
-._. . . ._.
-|_| |_| |.|
-|_| ..| |_|
-
-        ._.
-        |.|
-        |_|
-        ...
-        ..|
-        ..|
-        ._.
-        ._|
-        |_.
-        ._.
-        ._|
-        ._|
-        . .
-        |_|
-        ..|
-        ._.
-        |_.
-        ._|
-        ._.
-        |_.
-        |_|
-        ._.
-        ..|
-        ..|
-        ._.
-        |_|
-        |_|
-        ._.
-        |_|
-        ..|
-*/
