17c38a4d42f4e937f02c95d471945bfa8489aa1a
osxisl <olehumc@gmail.com>
2018-03-02 20:01:19 +0200

    Initial commit

---
+++ src/multTab.java
@@ -0,0 +1,11 @@
+public class multTab {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <10; i++){
+            for (int a=1; a<10; a++){
+                System.out.println(i + " * " + a+ " = " + i*a);
+            }
+            System.out.println();
+        }
+    }
+}
