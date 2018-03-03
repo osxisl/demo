1af49e0910ef8e322436edd947c71e153537a36e
osxisl <olehumc@gmail.com>
2018-03-03 09:36:07 +0200

    sevenDigitsDisplay split for two versions Long with manual input and int

--- src/sevenSegmDisplayLong.java
+++ src/sevenSegmDisplayLong.java
@@ -1,17 +1,22 @@
-public class sevenSegmDisplay {
+import java.util.Scanner;
+
+public class sevenSegmDisplayLong {
     public static void main(String[] args) {
-        int number = 1982736450;
+        System.out.println("Enter number to display: ");
+        Scanner in = new Scanner(System.in);
+        long number = in.nextLong();
 
         String numberStr = String.valueOf(number);
         int[] numberArray = new int [numberStr.length()];
         for (int i=0; i<numberStr.length(); i++) {
             numberArray[i] = Integer.parseInt( String.valueOf(numberStr.charAt(i)) );
         }
+
         String[][] symbolsArray = {
                 {"._. ", "... ", "._. ", "._. ", "... ", "._. ", "._. ", "._. ", "._. ", "._. "},
                 {"|.| ", "..| ", "._| ", "._| ", "|_| ", "|_. ", "|_. ", "..| ", "|_| ", "|_| "},
                 {"|_| ", "..| ", "|_. ", "._| ", "..| ", "._| ", "|_| ", "..| ", "|_| ", "..| "},
-                };
+        };
 
         for (int i=0; i<3; i++){
             for ( int DigitsNumber = 0; DigitsNumber<numberStr.length();DigitsNumber++) {
